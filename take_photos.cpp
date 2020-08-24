757fbb8b5444b00ce2db2ac8dc1597974764e59d
dontpanic123 <42323006+dontpanic123@users.noreply.github.com>
2020-08-24 12:41:00 +0200

    Update take_photos.cpp
    automatic take a photo

--- take_photos.cpp
+++ take_photos.cpp
@@ -1,26 +1,22 @@
-
-#include <opencv2/dnn.hpp>
 #include <librealsense2/rs.hpp>
 #include "../cv-helpers.hpp"
 #include <string>
 #include<sstream>
 #include <fstream>              // File IO
-#include <iostream>             // Terminal IO         
 #include <map>
-// 3rd party header for writing png files
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
 
-// Helper function for writing metadata to disk as a csv file
-void metadata_to_csv(const rs2::frame& frm, const std::string& filename);
 
 int main(int argc, char** argv) try
 {
     using namespace cv;
     using namespace rs2;
 
+   
+    
+    std::cout << "Press 's' key to save photos. " << std::endl;
+    
     rs2::context                          ctx;        // Create librealsense context for managing devices
-    std::map<std::string, rs2::colorizer> colorizers; // Declare map from device serial number to colorizer (utility class to convert depth data RGB colorspace)
+    std::map<std::string, rs2::colorizer> colorizers; // Declare map from device serial number to colorizer (utility cl  ass to convert depth data RGB colorspace)
     std::vector<rs2::pipeline>            pipelines;
     // Start a streaming pipe per each connected device
     for (auto&& dev : ctx.query_devices())
@@ -31,26 +27,30 @@ int main(int argc, char** argv) try
         cfg.enable_stream(RS2_STREAM_COLOR, 1280, 720, RS2_FORMAT_BGR8, 30);
         pipe.start(cfg);
         pipelines.emplace_back(pipe);
-        
-        // Map from each device's serial number to a different colorizer
-        colorizers[dev.get_info(RS2_CAMERA_INFO_SERIAL_NUMBER)] = rs2::colorizer();
-    }
-    int i = 0;//camera window's index
-    for (auto&& pipe : pipelines)
-    {
-        // Start streaming from Intel RealSense Camera
-       // pipeline pipe;
-        //rs2::config cfg;
-        //Add desired streams to configuration
-       // cfg.enable_stream(RS2_STREAM_COLOR, 1280, 720, RS2_FORMAT_BGR8, 30);
-       // auto config = pipe.start(cfg);
 
-        std:: string j = std::to_string(i);
-        i += 1;
-        const auto window_name = "camera"+j;
+        // Map from each device's serial number to a different colorizer
+        colorizers[dev.get_info(RS2_CAMERA_INFO_SERIAL_NUMBER)] = rs2::colorizer(); 
+    }
+
+
+    //Detect the number of connected cameras and generate a sufficient number of windows
+    const auto window_name = "camera0";
+    for (int w = 0; w != pipelines.size(); w++)
+    {
+        std::string j = std::to_string(w);
+        const auto window_name = "camera" + j;
         namedWindow(window_name, WINDOW_AUTOSIZE);
-        int  camcv_counter = 0;
-        while (getWindowProperty(window_name, WND_PROP_AUTOSIZE) >= 0)
+
+    }
+
+    int  camcv_counter = 0;
+    int photo_counter = 1;
+    double start_time = static_cast<double>(getTickCount());
+    while (getWindowProperty(window_name, WND_PROP_AUTOSIZE) >= 0)// This while loop is to refresh the video for each window
+    {
+        int w = 0;
+     
+        for (auto&& pipe : pipelines)//This for loop refresh only one frame for each window
         {
             // Wait for the next set of frames
             auto data = pipe.wait_for_frames();
@@ -68,58 +68,89 @@ int main(int argc, char** argv) try
             // Convert RealSense frame to OpenCV matrix:
             auto color_mat = frame_to_mat(color_frame);
             resize(color, resize1, Size(color.cols / 2, color.rows / 2), 0, 0, INTER_LINEAR);
+
+            //send photo to the right window
+            std::string j = std::to_string(w);
+            const auto window_name = "camera" + j;
             imshow(window_name, resize1);
-            //imwrite("0-1.png", color);
-            //std::cout << "Saved " << std::endl;
+            w += 1;
 
-           
-            if (waitKey(1) >= 0)
-            {
-                camcv_counter += 1;
-                std::string photo_name = camcv_counter + ".png";
-                //imwrite(photo_name, color_mat);
-                std::cout << "Saved " << std::endl;
-                break;
-            }
         }
-    }
 
-    rs2::colorizer color_map;
-    int cam_counter = 0, photo_counter = 12;
-    for (auto&& pipe : pipelines)
-    {   
-        for (auto i = 0; i < 30; ++i) pipe.wait_for_frames();
-        // Wait for the next set of frames from the camera. Now that autoexposure, etc.
-        // has settled, we will write these to disk
-        for (auto&& frame : pipe.wait_for_frames())
+        if (waitKey(1) == 115)// if "s" was drucked, one frame form each window will be saved.
+        {   
+                std::cout << "Please enter the photo counter: ";
+                
+                std::cin >> photo_counter;
+
+
+                int cam_counter = 0;
+                for (auto&& pipe : pipelines)
+                {
+                    // Wait for the next set of frames
+                    auto data = pipe.wait_for_frames();
+                    // Make sure the frames are spatially aligned
+
+                    auto color_frame = data.get_color_frame();
+                    Mat color(Size(1280, 720), CV_8UC3, (void*)color_frame.get_data(), Mat::AUTO_STEP);
+                    // If we only received new depth frame, 
+                    // but the color did not update, continue
+                    static int last_frame_number = 0;
+                    if (color_frame.get_frame_number() == last_frame_number) continue;
+                    last_frame_number = color_frame.get_frame_number();
+
+                    // Convert RealSense frame to OpenCV matrix:
+                    auto color_mat = frame_to_mat(color_frame);
+
+                    std::string photo_name = std::to_string(cam_counter) + "-" + std::to_string(photo_counter) + ".png";
+                    imwrite(photo_name, color_mat);
+                    std::cout << photo_name << "-Saved " << std::endl;
+                    cam_counter += 1;
+                }
+
+            
+        }
+       
+       double show_time = (static_cast<double>( getTickCount()) - start_time)/getTickFrequency();
+       //int s_time = int(show_time);
+       //double rest_time = s_time % 5;
+       if (show_time>=5)// If the show time of videos longer than 5 second, a photo will be saved.
         {
-            // We can only save video frames as pngs, so we skip the rest
-            if (auto vf = frame.as<rs2::video_frame>())
+
+            start_time = static_cast<double>(getTickCount()) ;//start time initialization
+            std::cout << "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$" << std::endl;// Allow the photographer to see the time point of the shooting
+            int cam_counter = 0;
+            for (auto&& pipe : pipelines)
             {
-                auto stream = frame.get_profile().stream_type();
-                // Use the colorizer to get an rgb image for the depth stream
-                
-                //vf = color_map.process(frame);
+                // Wait for the next set of frames
+                auto data = pipe.wait_for_frames();
+                // Make sure the frames are spatially aligned
+
+                auto color_frame = data.get_color_frame();
+                Mat color(Size(1280, 720), CV_8UC3, (void*)color_frame.get_data(), Mat::AUTO_STEP);
+                // If we only received new depth frame, 
+                // but the color did not update, continue
+                static int last_frame_number = 0;
+                if (color_frame.get_frame_number() == last_frame_number) continue;
+                last_frame_number = color_frame.get_frame_number();
+
+                // Convert RealSense frame to OpenCV matrix:
+                auto color_mat = frame_to_mat(color_frame);
+
+                std::string photo_name = std::to_string(cam_counter) + "-" + std::to_string(photo_counter) + ".png";
+                imwrite(photo_name, color_mat);
+                std::cout << photo_name << "-Saved " << std::endl;
                 cam_counter += 1;
-                
-                //imwrite(cam_counter+"-"+photo_counter,)
-                // Write images to disk
-                std::stringstream png_file;
-                png_file << "rs-save-to-disk-output-" << vf.get_profile().stream_name() << cam_counter << "-" << photo_counter << ".png";
-                stbi_write_png(png_file.str().c_str(), vf.get_width(), vf.get_height(),
-                    vf.get_bytes_per_pixel(), vf.get_data(), vf.get_stride_in_bytes());
-                std::cout << "Saved " << png_file.str() << std::endl;
-
-                // Record per-frame metadata for UVC streams
-                std::stringstream csv_file;
-                csv_file << "rs-save-to-disk-output-" << vf.get_profile().stream_name()
-                    << "-metadata.csv";
-                metadata_to_csv(vf, csv_file.str());
-
             }
+            photo_counter += 1;// To prevent the photo from being overwritten, update the name of the photo.
+
         }
+        
     }
 
+
+   
+    waitKey();
     return EXIT_SUCCESS;
 }
 catch (const rs2::error& e)
@@ -133,24 +164,3 @@ catch (const std::exception& e)
     return EXIT_FAILURE;
 }
 
-void metadata_to_csv(const rs2::frame& frm, const std::string& filename)
-{
-    std::ofstream csv;
-
-    csv.open(filename);
-
-    //    std::cout << "Writing metadata to " << filename << endl;
-    csv << "Stream," << rs2_stream_to_string(frm.get_profile().stream_type()) << "\nMetadata Attribute,Value\n";
-
-    // Record all the available metadata attributes
-    for (size_t i = 0; i < RS2_FRAME_METADATA_COUNT; i++)
-    {
-        if (frm.supports_frame_metadata((rs2_frame_metadata_value)i))
-        {
-            csv << rs2_frame_metadata_to_string((rs2_frame_metadata_value)i) << ","
-                << frm.get_frame_metadata((rs2_frame_metadata_value)i) << "\n";
-        }
-    }
-
-    csv.close();
-}
